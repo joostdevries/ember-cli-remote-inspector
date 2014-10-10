48cffcd7ade894d05f62c719a243f48f6fd14875
Joost de Vries <joost@closealert.com>
2014-10-10 15:59:10 +0200

    Pseudocode wip

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 module.exports = {
-  name: 'ember-cli-remote-inspector'
+  name: 'ember-cli-remote-inspector',
+
+  // Injects a script to allow for remote debugging
+  serverMiddleware: function(config) {
+    var addonContent = this;
+    var app = config.app;
+    var options = config.options;
+    var project = options.project;
+
+    app.use(function(req, res, next) {
+      var appConfig = project.config(options.environment);
+
+      if(isHTMLResponse(res)) {
+        injectRemoteDebugScript(res);
+      }
+
+      if(isDebuggerRequest(req)) {
+        renderDebugger(res);
+      }
+
+      next();
+    });
+  }  
 };
