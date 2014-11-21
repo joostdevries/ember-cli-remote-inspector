f72d5b855c8a78761f8b92999f910a0d8edc704e
Joost de Vries <joost@closealert.com>
2014-11-21 08:56:06 +0100

    Add option `remoteDebugScheme`, remove protocol relative urls, fixes #4

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
     var ENV = {
       remoteDebug: (environment==='development'),
       remoteDebugHost: 'localhost',
+      remoteDebugScheme: 'http',
       remoteDebugPort: 30820,
       remoteConsole: false
     }
@@ -14,19 +15,19 @@ module.exports = {
     return ENV;
   },
 
-  getRemoteDebugSocketScript: function(port, host) {
-    return '<script src="//' + host + ':' + port + '/socket.io/socket.io.js" type="text/javascript"></script>' +
+  getRemoteDebugSocketScript: function(port, host, scheme) {
+    return '<script src="' + scheme + '://' + host + ':' + port + '/socket.io/socket.io.js" type="text/javascript"></script>' +
            '<script type="text/javascript">' +
            '  window.EMBER_INSPECTOR_CONFIG = window.EMBER_INSPECTOR_CONFIG || {};' +
-           '  window.EMBER_INSPECTOR_CONFIG.remoteDebugSocket = io(\'http://' + host + ':'+port+'\');' +
+           '  window.EMBER_INSPECTOR_CONFIG.remoteDebugSocket = io(\'' + scheme + '://' + host + ':'+port+'\');' +
            '</script>';
   },
 
-  getEmberDebugScript: function(port, host) {
+  getEmberDebugScript: function(port, host, scheme) {
     return '<script type="text/javascript">' +
            '  (function(){' +
            '    var script = document.createElement(\'script\');' +
-           '    script.src = \'//' + host + ':' + port + '/ember_debug/ember_debug.js\';' +
+           '    script.src = \'' + scheme + '://' + host + ':' + port + '/ember_debug/ember_debug.js\';' +
            '    document.body.appendChild(script);' +
            '  })();' +
            '</script>';
@@ -47,9 +48,10 @@ module.exports = {
     }
 
     var port = process.env.EMBER_CLI_REMOTE_DEBUG_PORT = appConfig.remoteDebugPort,
-      host = process.env.EMBER_CLI_REMOTE_DEBUG_HOST = appConfig.remoteDebugHost;
+      host = process.env.EMBER_CLI_REMOTE_DEBUG_HOST = appConfig.remoteDebugHost,
+      scheme = process.env.EMBER_CLI_REMOTE_DEBUG_SCHEME = appConfig.remoteDebugScheme;
 
-    remoteDebugServer.setRemoteDebugSocketScript(this.getRemoteDebugSocketScript(port, host));
+    remoteDebugServer.setRemoteDebugSocketScript(this.getRemoteDebugSocketScript(port, host, scheme));
     remoteDebugServer.start(port, '0.0.0.0');
 
     // Currently only used to update the CSP header
@@ -59,8 +61,8 @@ module.exports = {
 
       if(cspHeader) {
         var cspHeaderValue = res.getHeader(cspHeader);
-        cspHeaderValue = cspHeaderValue.replace('connect-src \'self\' ', 'connect-src \'self\' http://'+host+':'+port+' ws://'+host+':'+port+' ')
-        cspHeaderValue = cspHeaderValue.replace('script-src \'self\' ', 'script-src \'self\' \'unsafe-inline\' http://'+host+':'+port+' ')
+        cspHeaderValue = cspHeaderValue.replace('connect-src \'self\' ', 'connect-src \'self\' '+scheme+'://'+host+':'+port+' ws://'+host+':'+port+' ')
+        cspHeaderValue = cspHeaderValue.replace('script-src \'self\' ', 'script-src \'self\' \'unsafe-inline\' '+scheme+'://'+host+':'+port+' ')
         res.setHeader(cspHeader, cspHeaderValue);
         res.setHeader('X-' + cspHeader, cspHeaderValue);
       }
@@ -71,10 +73,11 @@ module.exports = {
 
   contentFor: function(type) {
     var port = process.env.EMBER_CLI_REMOTE_DEBUG_PORT,
-      host = process.env.EMBER_CLI_REMOTE_DEBUG_HOST;
+      host = process.env.EMBER_CLI_REMOTE_DEBUG_HOST,
+      scheme = process.env.EMBER_CLI_REMOTE_DEBUG_SCHEME;
 
     if (type === 'body' && port && host) {
-      return this.getRemoteDebugSocketScript(port, host) + this.getEmberDebugScript(port, host);
+      return this.getRemoteDebugSocketScript(port, host, scheme) + this.getEmberDebugScript(port, host, scheme);
     }
   }
 };
